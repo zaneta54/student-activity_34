57de333697f01ccc2ca7a222f04c0744c412ba96
unknown <andrea_fuhrmann@hotmail.com>
2020-02-21 20:30:05 +0530

    added code for database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,40 @@
 var ball;
+var database,position,ballposition;
+
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database = firebase.database();
+    ballposition = database.ref('ball/position');
+    ballposition.on("value",readposition);
+
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+ database.ref('ball/position').set({ 'x' : position.x + x, 'y' : position.y + y });
+}
+function readposition(data){
+position =data.val();
+ball.x = position.x;
+ball.y = position.y;
 }
